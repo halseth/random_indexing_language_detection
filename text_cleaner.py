d3bd077006396e1d2e471e02289e52c466d37887
Johan Torås Halseth <johan@airbears2-10-142-39-44.AirBears2.1918.Berkeley.EDU>
2014-11-14 10:51:34 -0800

    Added the text cleaner script

---
+++ text_cleaner.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import sys
+import string
+import codecs
+
+from unidecode import unidecode
+
+include = string.lowercase
+exclude = string.punctuation
+
+if(len(sys.argv) < 4):
+    print "Usage: python script.py <inputFile.txt> <outputFile.txt> <language>"
+    exit()
+
+language = str(sys.argv[3])
+if(language == 'English'):
+    pass
+elif(language == 'Norwegian'):
+    include = include + 'æøå'
+else:
+    print('Unknown language. Exiting')
+    exit()
+
+inputFile = codecs.open(str(sys.argv[1]), encoding='utf-8')
+outputFile = open(str(sys.argv[2]), 'w')
+
+for s in inputFile:   
+    s = unidecode(s)
+    string_split = s.split()
+    new_s = ''
+
+    # check for all uppercase
+    for word in string_split:
+    		for ch in include:
+    				lowercase_exist = 0
+    				if ch in word:
+    						lowercase_exist = 1
+    						break
+    		num_exist = 0
+    		for num in '0123456789':
+    				if num in word:
+    						num_exist = 1
+    		if lowercase_exist and not num_exist:
+    				new_s += word + ' '
+    		else:
+    				new_s += ' '
+
+    # make all letters lowercase
+    new_s = new_s.lower()
+
+    # remove punctuation
+    t = ''.join(ch for ch in new_s if ch not in exclude)
+    outputFile.write(t+'\n')
+    
+inputFile.close()
+outputFile.close()
+       
