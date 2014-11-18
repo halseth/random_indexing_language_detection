b907607f555e81b32aecccf1dc7d5a669d627e45
adijos <dragon.federation.ii@gmail.com>
2014-11-18 00:17:20 -0800

    added experiment to sum all the clustering vectors

--- hyperdim.py
+++ hyperdim.py
@@ -7,17 +7,24 @@ import utils
 
 N = 100000 # dimension of random index vectors
 k = 1000 # number of + (or -)
-cluster = 3 # size of letter cluster
+cluster_max = 3 # size of max letter cluster
 languages = ['english','german','norwegian','finnish']
 
-
-for cluster in [1, 2, 3]:
+total_vectors = []
+for cluster in xrange(1,cluster_max+1):
 
 		print "~~~~~~~~~~"
 		# calculate language vectors
 		lang_vectors = RI.generate_vectors(N, k, cluster, languages=languages)
+		total_vectors.append(lang_vectors)
 
 		# print cosine angles 
 		print '=========='
 		print 'N = ' + str(N) + '; k = ' + str(k) + '; letters clusters are ' + str(cluster) + '\n'
 		cosangles = utils.cosangles(lang_vectors, languages)
+
+final_lang = sum(total_vectors)
+
+print '========='
+print 'N = ' + str(N) + '; k = ' + str(k) + '; max size letters clusters are ' + str(cluster) + '\n'
+cosangles = utils.cosangles(final_lang, languages)
