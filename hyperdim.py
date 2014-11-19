17801f3656b6f791513b9f41c45f5ae7a0a8e883
adijos <dragon.federation.ii@gmail.com>
2014-11-19 02:03:39 -0800

    added a lot of extra languages and some graphy visuals just for fun, no real value

--- hyperdim.py
+++ hyperdim.py
@@ -5,12 +5,13 @@
 import random_idx
 import utils
 import sys
+import scipy.io as scio
 
-N = 1000 # dimension of random index vectors
+N = 10000 # dimension of random index vectors
 k = 100 # number of + (or -)
 cluster_max = 2 # size of max letter cluste
 ordered_clusters=1
-languages = ['english','german','norwegian','finnish']
+languages = ['english','german','norwegian','finnish','dutch','french','afrikaans','danish','spanish']
 
 total_vectors = []
 unknown_tots = []
@@ -21,7 +22,7 @@ except IndexError:
 		unknown_txt = 'unknown1.txt'
 
 
-for cluster_sz in xrange(cluster_max,cluster_max+1):
+for cluster_sz in xrange(1,cluster_max+1):
 
 		# generate letter clusters and respective random indexing vectors
 		clusters, RI = random_idx.generate_id(N,k,cluster_sz=cluster_sz, ordered=ordered_clusters)
@@ -42,11 +43,12 @@ for cluster_sz in xrange(cluster_max,cluster_max+1):
 
 final_lang = sum(total_vectors)
 
-print '========='
-print 'N = ' + str(N) + '; k = ' + str(k) + '; max size letters clusters are ' + str(cluster_max) + '\n'
-cosangles = utils.cosangles(final_lang, languages)
 
 print '\n'
 # compare with "unknown text"
 final_unknown = sum(unknown_tots)
-utils.find_language(final_unknown, final_lang, languages)
+utils.find_language(unknown_txt, final_unknown, final_lang, languages)
+
+print '========='
+print 'N = ' + str(N) + '; k = ' + str(k) + '; max size letters clusters are ' + str(cluster_max) + '\n'
+cosangles = utils.cosangles(final_lang, languages,display=1)
--- text_cleaner.py
+++ text_cleaner.py
@@ -18,33 +18,43 @@ inputFile = codecs.open(str(sys.argv[1]), encoding='utf-8')
 outputFile = open(str(sys.argv[2]), 'w')
 
 for s in inputFile:
-    s = re.sub(u'ø', 'oe', s)
-    s = unidecode(s)
-    string_split = s.split()
-    new_s = ''
+		# fixed latin letter mappings
+		s = re.sub(u'ø', 'oe', s)
+		s = re.sub(u'å', 'aa', s)
+		s = re.sub(u'æ', 'ae', s)
+		s = re.sub(u'ä', 'ae', s)
+		s = re.sub(u'ö', 'oe', s)
+		s = re.sub(u'ü', 'ue', s)
+		s = re.sub(u'ß', 'ss', s)
 
-    # check for all uppercase
-    for word in string_split:
-    		for ch in include:
-    				lowercase_exist = 0
-    				if ch in word:
-    						lowercase_exist = 1
-    						break
-    		num_exist = 0
-    		for num in '0123456789':
-    				if num in word:
-    						num_exist = 1
-    		if lowercase_exist and not num_exist:
-    				new_s += word + ' '
-    		else:
-    				new_s += ' '
+		# decode the rest
+		s = unidecode(s)
 
-    # make all letters lowercase
-    new_s = new_s.lower()
+		string_split = s.split()
+		new_s = ''
 
-    # remove punctuation
-    t = ''.join(ch for ch in new_s if ch not in exclude)
-    outputFile.write(t+'\n')
+		# check for all uppercase
+		for word in string_split:
+				for ch in include:
+						lowercase_exist = 0
+						if ch in word:
+								lowercase_exist = 1
+								break
+				num_exist = 0
+				for num in '0123456789':
+						if num in word:
+								num_exist = 1
+				if lowercase_exist and not num_exist:
+						new_s += word + ' '
+				else:
+						new_s += ' '
+
+		# make all letters lowercase
+		new_s = new_s.lower()
+
+		# remove punctuation
+		t = ''.join(ch for ch in new_s if ch not in exclude)
+		outputFile.write(t+'\n')
     
 inputFile.close()
 outputFile.close()
