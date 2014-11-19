00f94775d702ccad076586072ff7726f9046ad62
Johan <johanth@gmail.com>
2014-11-19 12:28:28 -0800

    Added manual substitution of some characters to override unidecode

--- text_cleaner.py
+++ text_cleaner.py
@@ -18,43 +18,39 @@ inputFile = codecs.open(str(sys.argv[1]), encoding='utf-8')
 outputFile = open(str(sys.argv[2]), 'w')
 
 for s in inputFile:
-		# fixed latin letter mappings
-		s = re.sub(u'ø', 'oe', s)
-		s = re.sub(u'å', 'aa', s)
-		s = re.sub(u'æ', 'ae', s)
-		s = re.sub(u'ä', 'ae', s)
-		s = re.sub(u'ö', 'oe', s)
-		s = re.sub(u'ü', 'ue', s)
-		s = re.sub(u'ß', 'ss', s)
+    s = re.sub(u'ø', 'oe', s) // ø -> oe
+    s = re.sub(u'å', 'aa', s) // å -> aa
+    s = re.sub(u'æ', 'ae', s) // æ -> ae
+    s = re.sub(u'ä', 'ae', s) // ä -> ae
+    s = re.sub(u'ö', 'oe', s) // ö -> oe
+    s = re.sub(u'ü', 'ue', s) // ü -> ue
+    s = re.sub(u'ß', 'ss', s) // ß -> ss
+    s = unidecode(s)
+    string_split = s.split()
+    new_s = ''
 
-		# decode the rest
-		s = unidecode(s)
+    # check for all uppercase
+    for word in string_split:
+    		for ch in include:
+    				lowercase_exist = 0
+    				if ch in word:
+    						lowercase_exist = 1
+    						break
+    		num_exist = 0
+    		for num in '0123456789':
+    				if num in word:
+    						num_exist = 1
+    		if lowercase_exist and not num_exist:
+    				new_s += word + ' '
+    		else:
+    				new_s += ' '
 
-		string_split = s.split()
-		new_s = ''
+    # make all letters lowercase
+    new_s = new_s.lower()
 
-		# check for all uppercase
-		for word in string_split:
-				for ch in include:
-						lowercase_exist = 0
-						if ch in word:
-								lowercase_exist = 1
-								break
-				num_exist = 0
-				for num in '0123456789':
-						if num in word:
-								num_exist = 1
-				if lowercase_exist and not num_exist:
-						new_s += word + ' '
-				else:
-						new_s += ' '
-
-		# make all letters lowercase
-		new_s = new_s.lower()
-
-		# remove punctuation
-		t = ''.join(ch for ch in new_s if ch not in exclude)
-		outputFile.write(t+'\n')
+    # remove punctuation
+    t = ''.join(ch for ch in new_s if ch not in exclude)
+    outputFile.write(t+'\n')
     
 inputFile.close()
 outputFile.close()
