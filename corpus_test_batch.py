84d1f7d3ec1389a16e527230ec1fa08cbca50f79
Johan <johanth@gmail.com>
2014-12-14 12:13:47 -0800

    Updated corpus test batch to print the results mor readable

--- corpus_test_batch.py
+++ corpus_test_batch.py
@@ -16,13 +16,13 @@ main_base = os.getcwd()
 test_dir = '/lang_texts/test/processed_test'
 
 # parameters
-Ns = [1000, 2000, 5000, 8000, 10000] # dimension of random index vectors
-ks = [0.01, 0.1, 0.5] # number of + (or -)
+Ns = [1000] # dimension of random index vectors
+ks = [0.5] # number of + (or -)
 cluster_min = 1
 cluster_max = 5 # size of max letter cluster
 ordy = [0,1]
-#lang_map = {'af':'afr','bg':'bul','cs':'ces','da':'dan','nl':'nld','de':'deu','en':'eng','et':'est','fi':'fin','fr':'fra','el':'ell','hu':'hun','it':'ita','lv':'lav','lt':'lit','pl':'pol','pt':'por','ro':'ron','sk':'slk','sl':'slv','es':'spa','sv':'swe'}
-lang_map = {'af':'afrikaans','bg':'bulgarian','cs':'czech','da':'danish','nl':'dutch','de':'german','en':'english','et':'estonian','fi':'finnish','fr':'french','el':'greek','hu':'hungarian','it':'italian','pl':'polish','pt':'portuguese','ro':'romanian','sk':'slovak','sl':'slovenian','es':'spanish','sv':'swedish'}
+lang_map = {'af':'afr','bg':'bul','cs':'ces','da':'dan','nl':'nld','de':'deu','en':'eng','et':'est','fi':'fin','fr':'fra','el':'ell','hu':'hun','it':'ita','lv':'lav','lt':'lit','pl':'pol','pt':'por','ro':'ron','sk':'slk','sl':'slv','es':'spa','sv':'swe'}
+#lang_map = {'af':'afrikaans','bg':'bulgarian','cs':'czech','da':'danish','nl':'dutch','de':'german','en':'english','et':'estonian','fi':'finnish','fr':'french','el':'greek','hu':'hungarian','it':'italian','pl':'polish','pt':'portuguese','ro':'romanian','sk':'slovak','sl':'slovenian','es':'spanish','sv':'swedish'}
 languages = lang_map.values()
 #languages = ['french','italian','finnish','estonian']
 
@@ -36,25 +36,32 @@ for N in Ns:
 		cluster_sizes = xrange(cluster_min,cluster_max+1)
 		RI_letters = random_idx.generate_letter_id_vectors(N,k)
 
+		
 		##############################
 		# iterate over cluster sizes for language vectors ONLY
-		for cluster_sz in cluster_sizes:
-				for ordered in ordy:
-
-							print "~~~~~~~~~~"
+		for ordered in ordy:
+			
+				if ordered == 0:
+						ord_str = 'unordered'
+				else:
+						ord_str = 'ordered'
+			
+				print "N =", N, ", k =", k, ", ", ord_str
+				for cluster_sz in cluster_sizes:
+				
+							
+							##print "~~~~~~~~~~"
 							total_vectors = []
 							# calculate language vectors
 							lang_vectors = random_idx.generate_RI_lang(N, RI_letters, cluster_sz, ordered, languages=languages)
+							#lang_vectors = random_idx.generate_RI_lang_words(N, RI_letters, languages=languages)
 							total_vectors.append(lang_vectors)
 
 							# print cosine angles 
-							print '=========='
-							if ordered == 0:
-									ord_str = 'unordered!'
-							else:
-									ord_str = 'ordered!'
+							#print '=========='
+							
 
-							print 'N = ' + str(N) + '; k = ' + str(k) + '; letters clusters are ' + str(cluster_sz) + ', ' + ord_str + '\n'
+						#	print 'N = ' + str(N) + '; k = ' + str(k) + '; letters clusters are ' + str(cluster_sz) + ', ' + ord_str + '\n'
 							cosangles = utils.cosangles(lang_vectors, languages)
 							variance = utils.var_measure(cosangles)
 							#print "variance of language values: " + str(utils.var_measure(cosangles))
@@ -72,6 +79,7 @@ for N in Ns:
 									#print len(testf),testf[91:93]
 								#if testf == main_base + test_dir + '/da_432_p.txt':
 									unknown_vector = random_idx.generate_RI_text(N, RI_letters, cluster_sz, ordered,testf)
+									#unknown_vector = random_idx.generate_RI_text_words(N, RI_letters, testf)
 									unknown_tots.append(unknown_vector)
 									final_unknown = sum(unknown_tots)
 									likely_lang = utils.find_language(testf, final_unknown, final_lang, languages,display=0)
@@ -79,4 +87,5 @@ for N in Ns:
 									if lang_map[testf[91:93]] == likely_lang:
 											correct +=1
 
-							print "for size ", cluster_sz, " ordered=", ordered, " correct: ", correct, "; total: ", total,"; final percentage correct: ", float(correct)/total, " variance=",utils.var_measure(cosangles)
+							#print "for size ", cluster_sz, " ordered=", ordered, " correct: ", correct, "; total: ", total,"; final percentage correct: ", float(correct)/total, " variance=",utils.var_measure(cosangles)
+							print "cluster size", cluster_sz, ":", float(correct)/total, ", var",utils.var_measure(cosangles) 
